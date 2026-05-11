33ca4144cf960714a41a293f662ba94326ccda82
杏子 <zhongzhangjing@qq.com>
2026-05-11 15:47:05 +0800

    修改菜单warning

--- src/components/MenuCom.vue
+++ src/components/MenuCom.vue
@@ -1,18 +1,27 @@
 <script setup>
-import {useStore} from "vuex"
-import { computed,ref } from "vue";
-import { useRouter,useRoute } from "vue-router";
-const store=useStore();
-const router=useRouter();
-const route=useRoute();
+import { useStore } from "vuex"
+import { computed, ref } from "vue";
+import { useRouter, useRoute } from "vue-router";
+// 引入 Element Plus 图标
+import * as Icons from '@element-plus/icons-vue'
+import { House } from '@element-plus/icons-vue'
 
+const store = useStore();
+const router = useRouter();
+const route = useRoute();
 
-let menuDataCom=computed(()=>{
+let menuDataCom = computed(() => {
     return store.state.menus
 })
 
-let defaultActive=ref (route.path)
+let defaultActive = ref(route.path)
+
+// 图标字符串 转 组件（核心修复）
+const getIcon = (iconName) => {
+  return Icons[iconName] || null
+}
 </script>
+
 <template>
     <div class="menuCom">
         <el-menu 
@@ -25,32 +34,37 @@ let defaultActive=ref (route.path)
             <el-icon><House /></el-icon>
             <span>后台首页</span>
         </el-menu-item>
-            <template v-for="item in menuDataCom"    :key="item.id" >
-                <el-sub-menu v-if="item.id!=5"  :index="item.id + ''" >
+        
+            <template v-for="item in menuDataCom" :key="item.id">
+                <el-sub-menu v-if="item.id!=5" :index="item.id + ''">
                 <template #title>
                     <el-icon>
-                        <component :is="item.icon"></component>
+                      <!-- 修复这里 -->
+                      <component v-if="getIcon(item.icon)" :is="getIcon(item.icon)"></component>
                     </el-icon>
                     <span>{{ item.name }}</span>
                 </template>
-                <!--二级菜单-->
+
+                <!-- 二级菜单 -->
                 <el-menu-item 
                     v-for="subItem in item.child" 
-                    :index="'/admin'+subItem.frontpath" 
+                    :index="'/admin' + subItem.frontpath" 
                     :key="subItem.id"
                 >
                     <template #title>
                         <el-icon>
-                            <component :is="subItem.icon"></component>
+                          <!-- 修复这里 -->
+                          <component v-if="getIcon(subItem.icon)" :is="getIcon(subItem.icon)"></component>
                         </el-icon>
                         <span>{{ subItem.name }}</span>
                     </template>
                 </el-menu-item>
-            </el-sub-menu>  
-            </template>          
+            </el-sub-menu>
+            </template>
         </el-menu>
     </div>
 </template>
+
 <style scoped>
 :deep(.el-icon) {
   font-size: 25px !important;
